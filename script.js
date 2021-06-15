963adfd9ae97cc74b980d71e8b7afe509b3bafc4
Spryti <67284103+Spryti@users.noreply.github.com>
2021-06-15 20:49:03 +0000

    Finished code ;]

--- script.js
+++ script.js
@@ -15,15 +15,21 @@ function setup() {
 canvasDiv = createDiv();
 canvas = createCanvas(640, 480);
 background(100, 80, 200);
-canvas.mousePressed(drawEllipse);
+canvas.mousePressed(drawSplatter);
 canvas.parent(canvasDiv);
   // create slider UI
-
+  sliderDiv = createDiv();
+  minSpan = createSpan("Min");
+  minSpan.parent(sliderDiv);
+  slider = createSlider(0, 100, 50, 1);
+  slider.parent(sliderDiv);
+  maxSpan = createSpan("Max");
+  maxSpan.parent(sliderDiv);
   // create button UI
 buttonDiv = createDiv();
 clearButton = createButton("Clear Canvas");
 clearButton.mousePressed(function(){
-  background(65, 60, 88);
+  background(random(1, 100), random(1, 100), random(1, 100));
 });
 clearButton.parent(buttonDiv);
 }
@@ -55,5 +61,14 @@ ellipse(mouseX, mouseY, 100);
 *******************************************************************************/
 
 function drawSplatter(){
-
+  fill(random(100, 256), random(100, 256), random(100, 256));
+  noStroke();
+  let ellipses = random(15, 41);
+  let spread = slider.value();
+  for(let i = 0; i < ellipses; i++) {
+    let randomSize = random(5, 21);
+    let randomX = random(mouseX - spread, mouseX + spread + 1);
+    let randomY = random(mouseY - spread, mouseY + spread + 1);
+    ellipse(randomX, randomY, randomSize);
+}
 }
